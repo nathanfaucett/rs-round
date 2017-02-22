4239218a2697c52b52d8212a4878053505da9a3e
nathanfaucett <nathanfaucett@gmail.com>
2017-02-21 18:19:20 -0600

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -2,45 +2,7 @@
 #![no_std]
 
 
-use core::intrinsics::{roundf32, roundf64};
+mod round;
 
 
-pub trait Round {
-    fn round(self) -> Self;
-}
-
-macro_rules! trait_round {
-    ($t:ident) => (
-        impl Round for $t {
-            #[inline(always)]
-            fn round(self) -> $t { self }
-        }
-    );
-}
-
-trait_round!(usize);
-trait_round!(u8);
-trait_round!(u16);
-trait_round!(u32);
-trait_round!(u64);
-
-trait_round!(isize);
-trait_round!(i8);
-trait_round!(i16);
-trait_round!(i32);
-trait_round!(i64);
-
-impl Round for f32 {
-    #[inline(always)]
-    fn round(self) -> f32 { unsafe { roundf32(self) } }
-}
-impl Round for f64 {
-    #[inline(always)]
-    fn round(self) -> f64 { unsafe { roundf64(self) } }
-}
-
-#[test]
-fn test_round() {
-    assert_eq!((1.2f32).round(), 1f32);
-    assert_eq!((2).round(), 2);
-}
+pub use round::Round;
